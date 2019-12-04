dd1b6c02125e39bade31c6a235de7e8926577bba
Alex231 <git@alex231.com>
2019-12-04 22:49:35 +0000

    Presence ⭐ ❤️

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setPresence({ game: { name: 'the stars.', type: 'WATCHING' } })
 });
 
 client.on('message', msg => {
