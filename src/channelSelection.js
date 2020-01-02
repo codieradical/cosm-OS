f237725f51292d1f03502f4c4ad852555d8dbdbd
Alex231 <git@alex231.com>
2020-01-02 22:47:30 +0000

    Console chat prototype.

---
+++ src/channelSelection.js
@@ -0,0 +1,131 @@
+const _ = require('underscore');
+
+var selectedIndex = 0;
+var serverId = undefined;
+var channelId = undefined;
+var client = undefined;
+var resolve = undefined;
+
+function enterSelection(_client) {
+    client = _client;
+
+    var stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.resume();
+    stdin.setEncoding('utf8');
+
+    selectionChanged(null)
+    stdin.on('data', selectionChanged);
+
+    return new Promise(function(_resolve) {
+        resolve = _resolve;
+    });
+}
+
+function exitSelection() {
+    var stdin = process.stdin;
+    stdin.setRawMode(false);
+    stdin.pause();
+    stdin.removeListener('data', selectionChanged);
+    resolve(channelId)
+    //stdin.setEncoding('utf8');
+
+    //selectionChanged(null)
+    //stdin.on('data', selectionChanged);
+}
+
+function selectionChanged(key) {
+    console.clear();
+
+    console.log('Welcome to cosm-os.')
+
+    if (key == '\u001B\u005B\u0041') {
+        selectedIndex -= 1;
+        if (selectedIndex < 0)
+            selectedIndex = 0;
+    }
+    if (key == '\u001B\u005B\u0042') {
+        selectedIndex += 1;
+        serversCount = Array.from(client.guilds.values()).length;
+        if(!serverId && selectedIndex >= serversCount)
+            selectedIndex = serversCount - 1;
+        else if (serverId) {
+            textChannelsCount = Array.from(client.guilds.get(serverId).channels.values()).filter(channel => channel.type == 'text').length;
+            if(selectedIndex >= textChannelsCount)
+                selectedIndex = textChannelsCount - 1;
+        }
+    }
+    if (key == '\u0003') {
+        process.exit();
+    }    // ctrl-c
+    if (key == '\u0013') {
+        process.exit(); 
+    }
+    if (key == '\u000D\u000A' || key == '\u000D' || key == '\u000A' ) {
+        if(!serverId) {
+            serverId = Array.from(client.guilds.values())[selectedIndex].id;
+        } else {
+            channelId = Array.from(client.guilds.get(serverId).channels.values()).sort((a, b) => a.position - b.position).filter(channel => channel.type == 'text')[selectedIndex].id;
+            exitSelection();
+        }
+    }
+
+    if(!serverId)
+        printServers();
+    else
+        printChannels();
+}
+
+function printServers() {
+    console.log('Select a server:')
+    Array.from(client.guilds.values()).forEach((guild, index) => {
+        if(index === selectedIndex) {
+            console.log(`>${guild.name}<`)
+        } else {
+            console.log(guild.name);
+        }
+    })
+}
+
+function printChannels() {
+    console.log('Select a channel:')
+    var server = client.guilds.get(serverId);
+    var allChannels = Array.from(server.channels.values());
+    allChannels = allChannels.sort((a, b) => a.position - b.position);
+    var textChannels = allChannels.filter(channel => channel.type == 'text');
+    var categories = _.groupBy(textChannels, channel => channel.parentID);
+    
+    var currentCategoryIndex = 0;
+    var selectedCategoryIndex = 0;
+    var selectedChannelIndex = selectedIndex;
+    for (var categoryID in categories) {
+      if (categories[categoryID].length <= selectedChannelIndex && selectedCategoryIndex == currentCategoryIndex) {
+          selectedCategoryIndex += 1;
+          selectedChannelIndex -= categories[categoryID].length;
+      }
+  
+      if(categoryID == 'null') {
+        categories[categoryID].forEach((channel, index) => {
+            if(index === selectedChannelIndex && selectedCategoryIndex == currentCategoryIndex) {
+                console.log(`>#${channel.name}<`)
+            } else {
+                console.log(`#${channel.name}`)
+            }
+        })
+      } else {
+        console.log(server.channels.get(categoryID).name)
+        categories[categoryID].forEach((channel, index) => {
+            if(index === selectedChannelIndex && selectedCategoryIndex == currentCategoryIndex) {
+                console.log(`    >#${channel.name}<`)
+            } else {
+                console.log(`    #${channel.name}`)
+            }
+        })
+      }
+      currentCategoryIndex++;
+    }
+}
+
+module.exports = {
+    enterSelection
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const { enterSelection: selectChannel } = require('./channelSelection');
+var channelId = undefined;
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setPresence({ game: { name: 'the stars.', type: 'WATCHING' } })
+
+  selectChannel(client).then(_channelId => {
+    channelId = _channelId
+  
+    console.clear();
+    console.log(`Now chatting with ${client.guilds.get(client.channels.get(channelId).guild.id).name} in #${client.channels.get(channelId).name}`)
+    process.stdin.resume();
+    process.stdin.on('data', data => {
+      client.channels.get(channelId).send(data)
+    });
+  });
 });
 
 client.on('message', msg => {
@@ -12,13 +25,21 @@ client.on('message', msg => {
     if (msg.content === '❤️') {
         msg.channel.send('❤️'); 
     }
-    if (msg.content === 'cosm-os') {
-        msg.channel.send('hello, friend');
-    }
+
     if (msg.content.toLowerCase().replace('\'', '') === 'im a baa') {
         msg.channel.send('baa'); 
     }
-    console.log(`${msg.author.username}#${msg.author.discriminator}: ${msg.content}`);
+
+    // This command is very dangerous don't enable it unless you're stupid like me.
+    // - Codie
+    // if (msg.content.indexOf('eval ') > -1) {
+    //   eval(msg.content.replace('eval ', ''));
+    //   return;
+    // }
+
+    if(channelId && msg.channel.id === channelId) {
+      console.log(`${msg.author.username}#${msg.author.discriminator}: ${msg.content}`); 
+    }
 });
 
 client.login(process.env.BOT_TOKEN);
