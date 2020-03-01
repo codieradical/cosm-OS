91e5775376b65047e040a3654be63cd8fe33e0bb
Alex231 <git@alex231.com>
2020-03-01 16:35:10 +0000

    Announce Patreon posts.

--- src/server.js
+++ src/server.js
@@ -7,8 +7,11 @@ app.use(express.json());
 app.get('/', (req, res) => res.send('Welcome to Cosm-OS.'))
 
 app.post('/patreon/incoming', (req, res) => {
-    console.log(req.body);
-    client.channels.get(process.env.PATREON_CHANNEL_ID).send(JSON.stringify(req.body));
+    client.channels.get(process.env.PATREON_CHANNEL_ID).send(
+        "New Patreon Post\n" +
+        "**" + req.body.attributes.title + "**\n" +
+        "www.patreon.com/posts/" + req.body.id
+    )
     res.send();
 })
 
