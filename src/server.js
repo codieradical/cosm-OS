96152eed104db7e117a5e17dad5bd038d68ecd53
Alex231 <git@alex231.com>
2020-03-01 16:21:43 +0000

    Test webhook

--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
+const client = require('client');
 
 app.get('/', (req, res) => res.send('Welcome to Cosm-OS.'))
+
+app.post('/patreon/incoming', (req, res) => {
+    console.log(req);
+    client.channels.get(process.env.PATREON_CHANNEL_ID).send(JSON.stringify(req));
+    res.send();
+})
 
 app.listen(process.env.PORT, () => console.log(`Web server listening on ${process.env.PORT}.`))
