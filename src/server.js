cfb436032e8293c93f0d12543c6ee6e1c34ddccb
Codie <git@codie.gg>
2020-09-16 21:26:52 +0100

    Only mention patrons when there's a new post.

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => res.send('Welcome to Cosm-OS.'))
 app.post('/patreon/incoming', (req, res) => {
     console.log("Patreon Webhook Incoming", req.body.data)
     client.channels.get(process.env.PATREON_CHANNEL_ID).send(
-        "@everyone\n" +
+        "<@&755886739796656228>\n" +
         "New Patreon Post!\n" +
         "**" + req.body.data.attributes.title + "**\n" +
         "https://www.patreon.com/posts/" + req.body.data.id
